ea0af5e15840ce9ff894ed6d11ad80ad1d5e4a3e
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-05-13 22:11:18 -0400

    fixed script

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -12,3 +12,5 @@ document.querySelector("#currentyear").innerHTML = date.getFullYear();
 
 const button = document.querySelector(".ham");
 const navlist = document.querySelector(".navmenu");
+
+button.addEventListener("click",() =>{navlist.classList.toggle("responsive")}, false);
