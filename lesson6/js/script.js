8353df08f876745b58e356eeebcc79b996c65732
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-05-23 01:47:46 -0400

    upload

---
+++ lesson6/js/script.js
@@ -0,0 +1,19 @@
+const date = new Date();
+const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    };
+    document.querySelector(
+      "#currentdate"
+    ).innerHTML = new Date().toLocaleDateString("en-us", options);
+document.querySelector("#currentyear").innerHTML = date.getFullYear();
+
+const button = document.querySelector(".ham");
+const navlist = document.querySelector(".navmenu");
+
+button.addEventListener("click",() =>{navlist.classList.toggle("responsive")}, false);
+
+const headerchange = document.querySelector(".header");
+button.addEventListener("click", () =>{headerchange.classList.toggle("responsive")}, false);
