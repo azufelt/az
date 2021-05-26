6b9393fe5cd7c0efa998e74f4a3c07ecc8302d09
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-05-26 15:06:29 -0400

    lesson6

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,5 +3,13 @@
     const temp = document.querySelector("#hightemp").innerText;
     const speed = document.querySelector("#windspeed").innerText;
 
-    var chill = 35.74 + .6215 * temp - 35.75 * Math.pow(speed, .16) +  .4275 * temp * Math.pow(speed, .16);
-    document.querySelector("#windchill").innerHTML = chill.toFixed() + "°F";
+
+    var chill = 0;
+    var message = 0;
+    if (temp <= 50) {
+      chill = 35.74 + .6215 * temp - 35.75 * Math.pow(speed, .16) +  .4275 * temp * Math.pow(speed, .16);
+      message = chill.toFixed() + "°F";
+    } else {
+      message = "N/A"
+    }
+    document.querySelector("#windchill").innerHTML = message;
