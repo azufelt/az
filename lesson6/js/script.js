017e78f2b84157f5ef9bb46c31f338154e8f2083
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-05-28 22:47:36 -0400

    deleted old lesson 6

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -15,8 +15,9 @@ const navlist = document.querySelector(".navmenu");
 
 button.addEventListener("click",() =>{navlist.classList.toggle("responsive")}, false);
 
-const headerchange = document.querySelector(".header");
+const headerchange = document.querySelector("header");
 button.addEventListener("click", () =>{headerchange.classList.toggle("responsive")}, false);
+
 
 //display banner if day is friday
 if (new Date().getDay() == 5) {
@@ -28,9 +29,38 @@ if (new Date().getDay() == 5) {
 //check date, get number, add to number, compare to list, output message
 const today = date.getDay();
 const tomorrow = today + 1;
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+if (today == 3) {
+  document.querySelector(".forecastbox1").innerHTML = days[today];
+document.querySelector(".forecastbox2").innerHTML = days[today + 1];
+document.querySelector(".forecastbox3").innerHTML = days[today + 2];
+document.querySelector(".forecastbox4").innerHTML = days[today + 3];
+document.querySelector(".forecastbox5").innerHTML = days[today - 3];
+} else if (today == 4) {
+  document.querySelector(".forecastbox1").innerHTML = days[today];
+  document.querySelector(".forecastbox2").innerHTML = days[today + 1];
+  document.querySelector(".forecastbox3").innerHTML = days[today + 2];
+  document.querySelector(".forecastbox4").innerHTML = days[today - 4];
+  document.querySelector(".forecastbox5").innerHTML = days[today - 3];
+}
+else if (today == 5) {
+  document.querySelector(".forecastbox1").innerHTML = days[today];
+document.querySelector(".forecastbox2").innerHTML = days[today + 1];
+document.querySelector(".forecastbox3").innerHTML = days[today - 5];
+document.querySelector(".forecastbox4").innerHTML = days[today - 4];
+document.querySelector(".forecastbox5").innerHTML = days[today - 3];
+}
+else if (today == 6) {
+  document.querySelector(".forecastbox1").innerHTML = days[today];
+document.querySelector(".forecastbox2").innerHTML = days[today - 6];
+document.querySelector(".forecastbox3").innerHTML = days[today - 5];
+document.querySelector(".forecastbox4").innerHTML = days[today - 4];
+document.querySelector(".forecastbox5").innerHTML = days[today - 3];
+}
+else {
 document.querySelector(".forecastbox1").innerHTML = days[today];
 document.querySelector(".forecastbox2").innerHTML = days[today + 1];
 document.querySelector(".forecastbox3").innerHTML = days[today + 2];
 document.querySelector(".forecastbox4").innerHTML = days[today + 3];
 document.querySelector(".forecastbox5").innerHTML = days[today + 4];
+}
