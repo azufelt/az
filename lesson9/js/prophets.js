583ec679be23f544f2ee60481fca57120f3a19fd
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-06-18 18:31:59 -0400

    foreach

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,25 +5,28 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) {
+
+    prophets.forEach(prophet => {
       let card = document.createElement('section');
       let title = document.createElement('h2');
       let image = document.createElement('img');
       let birthdate = document.createElement('div');
       let birthplace = document.createElement('div');
-      title.innerHTML = prophets[i].name + " " + prophets[i].lastname;
-      birthdate.innerHTML = "Date of birth: " + prophets[i].birthdate;
-      birthplace.innerHTML = "Place of birth: " + prophets[i].birthplace;
+      title.innerHTML = prophet.name + " " + prophet.lastname;
+      birthdate.innerHTML = "Date of birth: " + prophet.birthdate;
+      birthplace.innerHTML = "Place of birth: " + prophet.birthplace;
       card.append(title);
       card.append(birthdate);
       card.append(birthplace);
       card.append(image);
-      image.setAttribute('src', prophets[i].imageurl);
-      image.setAttribute('alt', prophets[i].name + " " + prophets[i].lastname);
+      image.setAttribute('src', prophet.imageurl);
+      image.setAttribute('alt', prophet.name + " " + prophet.lastname);
       birthdate.setAttribute('class', 'birthdiv');
       birthplace.setAttribute('class', 'birthplacediv');
       document.querySelector('div.cards').append(card);
-  }
+
+    })
+    
 });
 
 
