7d35b69626c42ed7c5f4f13ce32475d540122c51
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-07-02 16:12:59 -0400

    validation check

--- drizzlybear/js/banner.js
+++
@@ -1,6 +0,0 @@
-//------display banner if day is friday--------//
-if (new Date().getDay() == 5) {
-  document.querySelector(".banner").style.display= "block";
-} else {
-  document.querySelector(".banner").style.display= "none";
-}
